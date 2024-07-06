8248761d6f66410d315811d9c049e856cad0dc15
github-ashwin <ashi.github@gmail.com>
2024-07-06 19:54:12 +0530

    Adding POST, GET, PUT and DELETE requests

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from typing import List,Optional
 from uuid import UUID, uuid4 # Unique identifier
@@ -14,9 +14,42 @@ class Task(BaseModel): # FastAPI will convert the pydantic model to JSON automat
 
 tasks = []
 
-@app.get('/')
-def read():
-    return {'hello':'word'}
+
+@app.post("/task/", response_model=Task) # POST request
+def create_task(task: Task):
+    task.id = uuid4()
+    tasks.append(task) # Appending to the memory db
+    return task
+
+@app.get("/task",response_model=List[Task]) # GET request
+def read_task():
+    return tasks
+
+@app.get("/task/{task.id}", response_model=Task) # GET a specific task
+def read_task(task_id: UUID):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+
+    return HTTPException(status_code=404, detail="Task not found")
+
+@app.put("/task/{task.id}",response_model=Task) # PUT request(updating)
+def update_task(task_id: UUID, task_update: Task):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = task.copy(update=task_update.dict(exclude_unset=True))
+            task[index] = updated_task
+            return updated_task
+        
+    raise HTTPException(status_code=404,detail="Task not found")
+
+@app.delete("/task/{task_id}",response_model=Task) # DELETE request
+def delete_task(task_id: UUID):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            return tasks.pop[index]
+        
+    raise HTTPException(status_code=404,detail="Task not found")
 
 if __name__ == "__main__":
     import uvicorn
